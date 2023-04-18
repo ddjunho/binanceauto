b98ce1e43dcd40812c4a3df54f5820478c811f26
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-04-18 20:48:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -51,7 +51,7 @@ def predict_target_price(ticker, target_type):
     # 데이터 불러오기
     candles = client.futures_klines(symbol=ticker, interval='4h', limit=1000)
     df = pd.DataFrame(candles, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'trades', 'taker_buy_base', 'taker_buy_quote', 'ignored'])
-    df = df.astype({'open': 'float', 'high': 'float', 'low': 'float', 'close': 'float', 'volume': 'float', 'quote_asset_volume': 'float', 'taker_buy_base': 'float', 'taker_buy_quote': 'float'})
+    df = df.astype({'open': 'float', 'high': 'float', 'low': 'float', 'close': 'float', 'volume': 'float', 'quote_asset_volume': 'float', 'taker_buy_base': 'float', 'taker_buy_quote': 'float', 'ignored': 'float'})
     print(df.dtypes)
     # 입력 데이터 전처리
     X = df
