4a0ea568dbdf733f1b0689d578b4b848341a410c
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-04-18 21:27:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -50,6 +50,7 @@ def get_current_price(ticker):
 def predict_target_price(ticker, target_type):
     # 데이터 불러오기
     candles = client.futures_klines(symbol=ticker, interval='4h', limit=1000)
+    df = pd.DataFrame()
     df = pd.DataFrame(candles, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'trades', 'taker_buy_base', 'taker_buy_quote', 'ignored'])
     df = df.astype({'open': 'float', 'high': 'float', 'low': 'float', 'close': 'float', 'volume': 'float', 'quote_asset_volume': 'float', 'taker_buy_base': 'float', 'taker_buy_quote': 'float'})
     df.pop('ignored')
