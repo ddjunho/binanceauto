8bb76da6033d6ccac12c04d5a765df9925b98556
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-04-27 14:07:19 +0900

    Update binanceautotrade2.py

--- binanceautotrade2.py
+++ binanceautotrade2.py
@@ -251,7 +251,7 @@ def job():
                     bull_market = True
                 else:
                     bull_market = False
-                message = f"매수가 조회 : {target_price}\n매도가 조회 : {sell_price}\n현재가 조회 : {current_price}\n1시간뒤 크거나 같을 확률 예측 : {hour_1*100}%\n2시간뒤 크거나 같을 확률 예측 : {hour_2*100}%\n4시간뒤 크거나 같을 확률 예측 : {hour_4*100}%\n6시간뒤 크거나 같을 확률 예측 : {hour_6*100}%\n8시간뒤 크거나 같을 확률 예측 : {hour_8*100}%\n매매조건 : {bull_market}\n내일 크거나 같을 확률{hour_24*100}%\n달러잔고 : {usd}\n비트코인잔고 : {btc}\n목표가 완화 : {PriceEase}\n레버리지 : {Leverage}"
+                message = f"매수가 조회 : {target_price}\n매도가 조회 : {sell_price}\n현재가 조회 : {current_price}\n1시간뒤 크거나 같을 확률 예측 : {hour_1*100}%\n2시간뒤 크거나 같을 확률 예측 : {hour_2*100}%\n4시간뒤 크거나 같을 확률 예측 : {hour_4*100}%\n6시간뒤 크거나 같을 확률 예측 : {hour_6*100}%\n8시간뒤 크거나 같을 확률 예측 : {hour_8*100}%\n매매조건 : {bull_market}\n조건무시 : {isForceStart}\n내일 크거나 같을 확률{hour_24*100}%\n달러잔고 : {usd}\n비트코인잔고 : {btc}\n목표가 완화 : {PriceEase}\n레버리지 : {Leverage}"
                 send_message(message)
                 start = False
             # 매수 조건
