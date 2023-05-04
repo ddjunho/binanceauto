8524fe980f98a56bf37fe7f63221d5062b122636
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-05-04 13:02:39 +0900

    Create best_k.py

---
+++ best_k.py
@@ -0,0 +1,18 @@
+from binance.client import Client
+import numpy as np
+
+
+def get_ror(k=0.5):
+    candles = client.futures_klines(symbol=ticker, interval='4h', limit=7)
+    df = pd.DataFrame(candles, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'trades', 'taker_buy_base', 'taker_buy_quote', 'ignored'])
+    df = df.astype({'open' : 'float', 'high' : 'float', 'low' : 'float', 'close' : 'float', 'volume' : 'float'})
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'], df['close'] / df['target'],1)
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+for k in np.arange(0.1, 1.0, 0.1):
+    ror = get_ror(k)
+    print("%.1f %f" % (k, ror))
