4662b299273f25deb6799747b4ff215c5fda43f1
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-05-21 23:07:13 +0900

    Update best_k.py

--- best_k.py
+++ best_k.py
@@ -21,8 +21,8 @@ def get_low_and_high(k_low=0.5, k_high=0.5):
 
 for k in np.arange(0.1, 1.0, 0.1):
     low, high = get_low_and_high(k_low=k)
-    print("%.1f %f %f" % (k, low, high))
+    print("%.1f %f %f" % (k, low))
 
 for k in np.arange(1.0, 2.0, 0.1):
     low, high = get_low_and_high(k_high=k)
-    print("%.1f %f %f" % (k, low, high))
+    print("%.1f %f %f" % (k, high))
