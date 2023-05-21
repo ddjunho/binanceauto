3e0c1cdfac9a7293bded955db513f0ff8a37f20b
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-05-21 22:42:04 +0900

    best_k.py

--- best_k.py
+++ best_k.py
@@ -5,17 +5,18 @@ from binance_keys import api_key, api_secret
 
 client = Client(api_key, api_secret)
 
-def get_ror(k=0.5):
-    candles = client.futures_klines(symbol="BTCUSDT", interval='4h', limit=7)
+def get_low_and_high(k=0.5):
+    candles = client.futures_klines(symbol="BTCUSDT", interval='4h', limit=6)
     df = pd.DataFrame(candles, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'trades', 'taker_buy_base', 'taker_buy_quote', 'ignored'])
     df = df.astype({'open' : 'float', 'high' : 'float', 'low' : 'float', 'close' : 'float', 'volume' : 'float'})
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
-    df['ror'] = np.where(df['high'] > df['target'], df['close'] / df['target'],1)
-    ror = df['ror'].cumprod().iloc[-2]
-    return ror
+    df['low'] = np.where(df['low'] < df['target'], df['close'] / df['target'],1)
+    low = df['low'].cumprod().iloc[-2]
+    high = df['high'].max() 
+    return low, high
 
 for k in np.arange(0.1, 1.0, 0.1):
-    ror = get_ror(k)
-    print("%.1f %f" % (k, ror))
+    low, high = get_low_and_high(k)
+    print("%.1f %f %f" % (k, low, high))
